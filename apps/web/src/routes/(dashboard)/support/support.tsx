83004e4ed51e801cc7b6b78bea4417f145f35172
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-07-08 22:36:05 +0100

    moved

--- apps/web/src/routes/(dashboard)/support/support.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(dashboard)/support/support')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(dashboard)/support/support"!</div>
-}
