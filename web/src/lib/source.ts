6e309a4b7b897d21d04e2bb0bbe5c004fec721da
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-08-10 21:48:26 +0100

    refactor: remove unused fumadocs components and related code from the project

--- web/src/lib/source.ts
+++
@@ -1,9 +0,0 @@
-import { loader } from 'fumadocs-core/source';
-import { docs } from '../../.source/server';
-
-export { docs };
-
-export const source = loader({
-  baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
-});
